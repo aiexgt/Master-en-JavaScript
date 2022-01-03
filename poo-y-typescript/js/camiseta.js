7e978c279f13b2290f35d75122b18a9d832600bd
aiexgt <lima14florian14@gmail.com>
2022-01-03 01:16:05 -0600

    Programacion orientada a objetos

---
+++ poo-y-typescript/js/camiseta.js
@@ -0,0 +1,53 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// Clase (molde del objeto)
+var Camiseta = /** @class */ (function () {
+    // Métodos (funciones o acciones del objeto)
+    function Camiseta(color, modelo, marca, talla, precio) {
+        this.color = color;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.talla = talla;
+        this.precio = precio;
+    }
+    Camiseta.prototype.setColor = function (color) {
+        this.color = color;
+    };
+    Camiseta.prototype.getColor = function () {
+        return this.color;
+    };
+    return Camiseta;
+}());
+// Clase hija
+var Sudadera = /** @class */ (function (_super) {
+    __extends(Sudadera, _super);
+    function Sudadera() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Sudadera.prototype.setCapucha = function (capucha) {
+        this.capucha = capucha;
+    };
+    Sudadera.prototype.getCapucha = function () {
+        return this.capucha;
+    };
+    return Sudadera;
+}(Camiseta));
+var camiseta = new Camiseta("uno", "dos", "tres", "cuatro", 5);
+console.log(camiseta);
+var sudadera_nike = new Sudadera("Rojo", "Manga Larga", "Nike", "L", 30);
+sudadera_nike.setCapucha(true);
+sudadera_nike.setColor("Azul");
+console.log(sudadera_nike);
